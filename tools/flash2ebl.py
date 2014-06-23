d699c5a03368b2dbc4f781c5269d48c1ebdebfaf
Felix Domke <tmbinc@elitedvb.net>
2014-06-23 20:05:17 +0200

    EM358x support

--- tools/flash2ebl.py
+++ tools/flash2ebl.py
@@ -2,46 +2,66 @@ import sys, zlib, struct
 
 # converts an EM35x flash image (dumped from 0x08000000, for example via SWI) to an .ebl
 
+is_em358x = True
+
 flash = open(sys.argv[1], "rb")
 
-pagen = 0
-
+BOOTLOADER_SIZE = 0x4000 if is_em358x else 0x2000
+FLASH_SIZE = 512*1024 if is_em358x else 192*1024
+PAGE_SIZE = 0x800
 EBLTAG_HEADER, EBLTAG_PROG, EBLTAG_ERASEPROG, EBLTAG_END = 0, 0xFE01, 0xFD03, 0xFC04
+
+BOOTLOADER_PAGES = BOOTLOADER_SIZE / PAGE_SIZE
 
 alldata = ""
 
 image = ""
 
-for i in range(0x2E000/0x800):
-	page = flash.read(0x800)
+for i in range(FLASH_SIZE/PAGE_SIZE):
+	if i >= BOOTLOADER_PAGES or True:
+		page = flash.read(PAGE_SIZE)
+
+	if i < BOOTLOADER_PAGES: # skip loader
+		continue
 
 	if not len(page):
 		break
 
-	if i < 4: # skip loader
-		continue
-
-	if page == ("\xFF" * 0x800):
+	if page == ("\xFF" * PAGE_SIZE):
 		break
 
-	if i == 4: # header
+	if len(page) != 0x800:
+		page += "\xFF" * (PAGE_SIZE - len(page))
+
+	if i == BOOTLOADER_PAGES: # header
 		aatBuff = page[:0x80]
 		page = page[0x80:]
 		image += "\xFF" * 0x80 + page
 	else:
 		image += page
 
-	alldata += struct.pack(">HHI", EBLTAG_ERASEPROG, len(page) + 4, 0x08000000 + i * 0x800 + 0x800 - len(page))
+	alldata += struct.pack(">HHI", EBLTAG_ERASEPROG, len(page) + 4, 0x08000000 + i * PAGE_SIZE + PAGE_SIZE - len(page))
 	alldata += page
 
 version=0x0201 
-signature=0xe350 
-flashAddr=0x08002000
+signature=0xe350
+flashAddr=0x08000000 + BOOTLOADER_SIZE
 
 baseTable, platInfo, microInfo, phyInfo, aatSize, softwareVersion, reserved, timestamp, imageInfo, imageCrc, pageRanges = struct.unpack("<24sBBBBHHI32sI12s", aatBuff[:0x54])
 
+assert aatSize == 0xAC
+assert struct.unpack("<H", baseTable[0x10:0x12])[0] == 0xAA7
+assert struct.unpack("<H", baseTable[0x12:0x14])[0] & 0xFF00 == 0x100
+assert platInfo == 4
+#print "microInfo %x" % microInfo
+#assert microInfo in [6,8]
+microInfo = 6
+
 imageCrc = zlib.crc32(image) & 0xFFFFFFFF
-pageRanges = chr(4) + chr(i-1) + ("\xFF" * 10)
+
+# pageRanges is a series of 12 (skip,check) tuples. We skip the header, but check the image.
+
+pageRanges = chr(BOOTLOADER_PAGES) + chr(i-1) + ("\xFF" * 10)
 
 aatBuff = struct.pack("<24sBBBBHHI32sI12s", baseTable, platInfo, microInfo, phyInfo, aatSize, softwareVersion, reserved, timestamp, imageInfo, imageCrc, pageRanges) + aatBuff[0x54:]
 
